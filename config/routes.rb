17a2661e220c4a163b0b39f9c5877843dced3c45
EC2 Default User <ec2-user@ip-172-31-17-59.ap-northeast-1.compute.internal>
2024-03-11 15:39:30 +0000

    started log_in page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+    root to: 'homes#top'
+  end 
+  
+  scope module: :public do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
