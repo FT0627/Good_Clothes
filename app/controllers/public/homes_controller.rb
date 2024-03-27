6cd4563c1882cca1260bdd4c909f8fd297ad8b4c
EC2 Default User <ec2-user@ip-172-31-17-59.ap-northeast-1.compute.internal>
2024-03-27 15:48:47 +0000

    made top serch-prefecture

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -6,6 +6,8 @@ class Public::HomesController < ApplicationController
       @tag_id = params[:tag_id]
       @tag = Tag.find(@tag_id)
       @items = @tag.items
+    elsif params[:prefecture].present?
+      @items = Shop.joins(:items).where(prefecture: Shop.prefectures[params[:prefecture].to_sym]).first.items
     else
       @items = Item.all
     end
