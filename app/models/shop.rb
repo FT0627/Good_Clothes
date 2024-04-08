56c757f8a40ab52730132b55d46a87915494e706
EC2 Default User <ec2-user@ip-172-31-17-59.ap-northeast-1.compute.internal>
2024-04-08 15:11:29 +0000

    made public shop item map, layout item_image

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -27,7 +27,7 @@ class Shop < ApplicationRecord
      }
 
    def get_shop_image(width, height)
-    unless item_image.attached?
+    unless shop_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       shop_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
