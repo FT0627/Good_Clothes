7e5ba9ccf2d7a7e650de33901f523e045dcc408c
FT0627 <fairytail1926@gmail.com>
2024-09-05 14:53:03 +0000

    added ajax on the shop favorite page

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,14 +4,14 @@ class Public::FavoritesController < ApplicationController
     shop = Shop.find(params[:shop_id])
     favorite = current_user.favorites.new(shop_id: shop.id)
     favorite.save
-    redirect_to shop_path(shop)
+    redirect_to request.referer
   end
   
   def destroy
     shop = Shop.find(params[:shop_id])
     favorite = current_user.favorites.find_by(shop_id: shop.id)
     favorite.destroy
-    redirect_to shop_path(shop)
+    redirect_to request.referer
   end
   
 end
